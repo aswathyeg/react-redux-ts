75f16f78e2526ad855e1b67e89871f80396711ca
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-02 17:39:45 +0530

    created store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
-import {combineReducers} from 'redux';
+import {combineReducers, createStore, Store} from 'redux';
 import userEventsReducer from './user-events';
 
 const rootReducer=combineReducers({
     userEvents:userEventsReducer
 })
+const store=createStore(rootReducer);
+export default store;
