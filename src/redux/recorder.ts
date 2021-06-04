4b45a7381de2669a7aa8b71663566f9c64371bf2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-04 15:36:14 +0530

    added star and stop function

--- src/redux/recorder.ts
+++ src/redux/recorder.ts
@@ -1,4 +1,27 @@
-const recorderReducer=(state,action)=>{
+import { type } from "os";
+import { Action } from "redux";
+
+interface RecorderState{
+    dateStart:string;
+} //for state
+
+//for 2 types of actions
+const START='recorder/start';
+const STOP='recoder/stop';
+
+type StartAction=Action<typeof START>;
+type StopAction=Action<typeof STOP>;
+
+const start=(): StartAction=>({
+   type:START
+
+});
+const stop=(): StopAction=>({
+    type:STOP
+ 
+ });
+
+const recorderReducer=(state:RecorderState,action)=>{
     switch (action.type){
        default:
         return state;
