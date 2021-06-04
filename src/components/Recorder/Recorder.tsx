9ab78afe6bd23945238f0fa33c54ec9e4dde09f2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-04 19:53:41 +0530

    added handleClick and dispatch

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { start } from '../../redux/recorder';
 import './Recorder.css';
 const Recorder=()=>{
+   const dispatch=useDispatch();
+   const handleClick=()=>{
+    dispatch(start());
+    }
 return(
     <div className="recorder">
-        <button className="recorder-record">
+        <button onClick={handleClick} className="recorder-record">
             <span></span>
             </button>
             <div className="recorder-counter">00:00:00</div>
