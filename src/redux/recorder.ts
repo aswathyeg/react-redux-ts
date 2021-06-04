22c9619103edf4174ffef37d8e2f66fe05d6900a
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-04 19:45:53 +0530

    formatted

--- src/redux/recorder.ts
+++ src/redux/recorder.ts
@@ -1,41 +1,41 @@
 import { type } from "os";
 import { Action } from "redux";
 
-interface RecorderState{
-    dateStart:string;
+interface RecorderState {
+    dateStart: string;
 } //for state
 
 //for 2 types of actions
-const START='recorder/start';
-const STOP='recoder/stop';
+const START = 'recorder/start';
+const STOP = 'recoder/stop';
 
-type StartAction=Action<typeof START>;
-type StopAction=Action<typeof STOP>;
+type StartAction = Action<typeof START>;
+type StopAction = Action<typeof STOP>;
 
 //action creaters
- export const start=(): StartAction=>({
-   type:START
+export const start = (): StartAction => ({
+    type: START
 
 });
-export const stop=(): StopAction=>({
-    type:STOP
- 
- });
+export const stop = (): StopAction => ({
+    type: STOP
 
- const initialState:RecorderState={
-    dateStart:''
- } //
+});
 
-const recorderReducer=(state:RecorderState =initialState,
-    action:StartAction|StopAction)=>{
-    switch (action.type){
+const initialState: RecorderState = {
+    dateStart: ''
+} //
+
+const recorderReducer = (state: RecorderState = initialState,
+    action: StartAction | StopAction) => {
+    switch (action.type) {
         case START:
-         return {...state,dateStart:new Date().toISOString()};
-         case STOP:
-            return {...state,dateStart:''};
-            }
-       default:
-        return state;
+            return { ...state, dateStart: new Date().toISOString() };
+        case STOP:
+            return { ...state, dateStart: '' };
+
+        default:
+            return state;
     };
 
 }
