034792dcf159ecbd14baa8c1bd24a5463f133b3c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-05 17:17:23 +0530

    added usesselector

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,11 +1,17 @@
+import classNames from 'classnames';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { start } from '../../redux/recorder';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectDateStart, start } from '../../redux/recorder';
 import './Recorder.css';
+import cx from 'classnames';
 const Recorder=()=>{
    const dispatch=useDispatch();
+   const dateStart=useSelector(selectDateStart);//to read state
+   const started=dateStart!='';
+
    const handleClick=()=>{
     dispatch(start());
+
     }
 return(
     <div className="recorder">
