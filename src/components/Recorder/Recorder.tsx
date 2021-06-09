ceeb7b0c2a3c002b377c00ad4e8e588f0c0bc38d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-09 21:18:59 +0530

    added time to jsx

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -30,12 +30,16 @@ let seconds=started? Math.floor((Date.now()-new Date(dateStart).getTime())/1000)
 
 const hours=seconds ? Math.floor (seconds / 60 / 60):0;
 seconds-=hours*60*60;
+const minutes=seconds ? Math.floor (seconds / 60 ):0;
+seconds-=minutes*60;
+
+
 return(
     <div className={cx('recorder',{'recorder-started':started})}  >
         <button onClick={handleClick} className="recorder-record">
             <span></span>
             </button>
-            <div className="recorder-counter">00:00:00</div>
+            <div className="recorder-counter">{hours}:{minutes}:{seconds}</div>
 
     </div>
 )
