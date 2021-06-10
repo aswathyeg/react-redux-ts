88a2f9dd0e007365ad6f3cf59bda94651383db3e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-10 19:43:27 +0530

    added redux-thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,12 @@
-import {combineReducers, createStore, Store} from 'redux';
+import {applyMiddleware, combineReducers, createStore, Store} from 'redux';
 import recorderReducer from './recorder';
 import userEventsReducer from './user-events';
+import thunk from 'redux-thunk';
 
 const rootReducer=combineReducers({
     userEvents:userEventsReducer,
     recorder:recorderReducer
 })
 export type RootState=ReturnType <typeof rootReducer>;
-const store=createStore(rootReducer);
+const store=createStore(rootReducer,applyMiddleware(thunk));
 export default store;
