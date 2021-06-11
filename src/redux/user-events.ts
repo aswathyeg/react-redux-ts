76d9defcbc4a84227ebd5a7ff1a754c79a867850
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-11 06:20:42 +0530

    added loadrequest

--- src/redux/user-events.ts
+++ src/redux/user-events.ts
@@ -1,4 +1,6 @@
-import { AnyAction } from "redux";
+import { Action, AnyAction } from "redux";
+import { ThunkAction } from "redux-thunk";
+import { RootState } from "./store";
 
 interface UserEvents{
     id:number;
@@ -15,6 +17,27 @@ const initialState:UserEventsState={
     byIds:{}  ,
     allIds:[]
 }
+const LOAD_REQUIEST='userEvents/load_request';
+interface LoadRequestAction extends Action<typeof LOAD_REQUIEST>{}
+
+export const loadUserEvents=():ThunkAction<
+void,
+RootState,
+undefined,
+LoadRequestAction
+ > => async(dispatch,getState)=>{
+    dispatch({
+        type:LOAD_REQUIEST
+    });
+    try{
+const response=await fetch('http://localhost:3001/events');
+const events:UserEvents[]=await response.json();
+    }
+    catch(e){
+
+    }
+
+};
 
 
 const userEventsReducer=(state:UserEventsState=initialState
