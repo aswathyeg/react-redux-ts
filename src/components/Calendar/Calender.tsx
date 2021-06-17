f088a3988cb59abd8325805cb54986d81f34b276
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-17 10:13:25 +0530

    removed the jsx part

--- src/components/Calendar/Calender.tsx
+++ src/components/Calendar/Calender.tsx
@@ -87,17 +87,7 @@ const Calendar: React.FC<Props> = ({ events, loadUserEvents }) => {
             </div>
             <div className="calendar-events">
               {events.map(event => {
-                return (
-                  <div className="calendar-event">
-                    <div className="calendar-event-info">
-                      <div className="calendar-event-time">10:00 - 12:00</div>
-                      <div className="calendar-event-title">{event.title}</div>
-                    </div>
-                    <button className="calendar-event-delete-button">
-                      &times;
-                    </button>
-                  </div>
-                );
+                
               })}
             </div>
           </div>
